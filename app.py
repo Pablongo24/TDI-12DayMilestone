ae5d63a042a679ebea0eaeba1ea411d0660f99b6
Pablongo24 <pablovega@live.com>
2019-01-02 21:55:01 -0500

    Moved modified index.html to /templates_tinkering and modified the original one to show what's actually running in Heroku.

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import re
 import io
 
 from bokeh.embed import components
-from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter
+from bokeh.models import ColumnDataSource, HoverTool, NumeralTickFormatter, CrosshairTool
 from bokeh.plotting import figure
 from bokeh.resources import INLINE
 from bokeh.util.string import encode_utf8
@@ -69,6 +69,8 @@ def create_plot(df, ticker, lines):
         'low':    {'color': 'black',  'legend': 'Low'},
     }
 
+    # print(df.head(15))
+
     title = ticker
     source = ColumnDataSource(df)
     hover_tools = []
@@ -89,9 +91,49 @@ def create_plot(df, ticker, lines):
     return script, div
 
 
+# Candlestick charts.
+# Notes:
+# 1) Code below based on # http://perfspy.blogspot.com/2017/03/candlestick-chart-using-boken-without.html
+# 2) For now I don't care about having date gaps in the chart (this is just me playing with bokeh in web apps)
+def create_candlesctick(df, ticker):
+    title = ticker
+
+    df['mid'] = df.apply(lambda x: (x['open'] + x['close'])/2, axis=1)
+    df['height'] = df.apply(lambda x: abs(x['close']-x['open']) if x['close'] != x['open'] else 0.001, axis=1)
+
+    inc = df.close > df.open
+    dec = df.open > df.close
+    width = 0.3
+
+    source_inc = ColumnDataSource(df.loc[inc])
+    source_dec = ColumnDataSource(df.loc[dec])
+    hover_tools = HoverTool(
+        tooltips=[
+            ('date', '@date'),
+            ('open', '@open'),
+            ('close', '@close'),
+        ])
+
+    tools = (CrosshairTool(), hover_tools)
+
+    p = figure(plot_width=800, plot_height=300, tools=tools, x_axis_type='datetime', title=title)
+
+    # Style candlesticks:
+    p.segment(df.timestamp[inc], df.high[inc], df.timestamp[inc], df.low[inc], color='green')  # Positive return
+    p.segment(df.timestamp[dec], df.high[dec], df.timestamp[dec], df.low[dec], color='green')  # Negative return
+    p.rect(x='timestamp', y='mid', width=width, height=height, fill_color='green', source=source_inc)  # Positive return
+    p.rect(x='timestamp', y='mid', width=width, height=height, fill_color='red', source=source_dec)  # Positive return
+    p.yaxis[0].formatter = NumeralTickFormatter(format="$0.00")
+    p.grid.grid_line_color = None
+
+    script, div = components(p)
+
+    return script, div
+
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    #Static resources:
+    # Static resources:
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
     plot_script = ""
@@ -105,11 +147,15 @@ def index():
         high = request.form.get('high')
         low = request.form.get('low')
 
-
         # Get stock info from Alpha Vantage
         df = alpha_vantage(ticker)
         if df is not None:
             plot_script, plot_div = create_plot(df, ticker, [opening, close, high, low])
+
+            # TODO: the function below isn't working, Need to figure out how to get input from multiple forms.
+            #  Use the index.html file in /template_tinkering and reference:
+            #  https://github.com/thedataincubator/sql_heroku/blob/master/app/templates/index.html#L54
+            #plot_script, plot_div = create_candlesctick(df, ticker)
         else:
             flash('Stock ticker not found')
             return redirect(url_for('index'))
@@ -131,4 +177,4 @@ def about():
 
 
 if __name__ == '__main__':
-    app.run(port=33507)
+    app.run(debug=True, use_debugger=True, use_reloader=True, passthrough_errors=False, port=33507)
